9210555294ed4573c6df787b5fe1a5fc07cfa512
actlikemunyao <munyaopapain@gmail.com>
2025-08-31 21:03:45 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -4,3 +4,5 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "graphql_crm.settings")
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
+    
+["import sys"]
